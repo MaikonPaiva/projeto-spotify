7c8be7ae8be90eed5f69d670e9c5072be5702da7
Maikon Paiva <maikonpaiva.dev@gmail.com>
2024-06-27 10:08:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
+//array
+let musicas = [
+    {titulo: 'guitar solo',
+     artista:'Maikon',
+     src:'musicas/Brontosaurus - Topher Mohr and Alex Elena.mp3',
+     img:'imagens/leutrim.jpg',  
+    },
+    {titulo: 'rock',
+        artista:'bon',
+        src:'musicas/Engine Fire - Silent Partner.mp3',
+        img:'imagens/giancarlo.jpg',  
+    },
+    {titulo: 'hit',
+        artista:'jov',
+        src:'musicas/Drop and Roll - Silent Partner.mp3',
+        img:'imagens/apolo.jpg',  
+    },
+];
+//Variaveis
 let musica = document.querySelector('audio');
 let duracao = document.querySelector('.fim');
+let imagem = document.querySelector("img");
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+let indexMusica = 0;
 
- 
+renderizarMusica(indexMusica);
 
+//Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
 document.querySelector('.botao-pause').addEventListener('click',pausarMusica);
 
 musica.addEventListener('timeupdate', atualizarBarra);
+
+document.querySelector('.anterior').addEventListener('click',() => {
+    indexMusica--;
+    if(indexMusica < 0){
+        indexMusica = 2;};
+    renderizarMusica(indexMusica);
+    tocarMusica()
+});
+
+document.querySelector('.proxima').addEventListener('click',() => {
+    indexMusica++;
+    if(indexMusica > 2){
+        indexMusica = 0};
+    renderizarMusica(indexMusica);
+    tocarMusica()
+
+});
+
+//Function
+function renderizarMusica(index){
+musica.setAttribute('src',musicas[index].src);
+musica.addEventListener('loadeddata', () => {
+    nomeMusica.textContent = musicas[index].titulo;
+    nomeArtista.textContent = musicas[index].artista;
+    imagem.src = musicas[index].img
+    duracao.textContent = segundosParaMinutos(Math.floor(musica.duration));
+
+
+});
+}
 
 function tocarMusica(){
     musica.play();
@@ -28,8 +83,6 @@ function atualizarBarra(){
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100 ) + '%';
     let tempoDecorrido = document.querySelector('.inicio');
     tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
-    let duracao = document.querySelector('.fim');
-    duracao.textContent = segundosParaMinutos(Math.floor(musica.duration));
     
 }
 function segundosParaMinutos(segundos){
@@ -41,4 +94,5 @@ function segundosParaMinutos(segundos){
 
     return campoMinutos+':'+campoSegundos;
 }
+
 
